0f245eaf82993c151a67a486aad708874f16c5c3
Uzair Khan <your_email@example.com>
2026-08-05 15:25:32 +0500

    Updation in script file

--- script.js
+++ script.js
@@ -1,160 +1,160 @@
-// for(let i = 1; i <= 20; i++){
-//     console.log(i);
-// }
-// for(let i = 1; i <= 20; i++){
-//     if(i % 2 == 0){
-//         console.log(i);
-//     }
-// }
+for(let i = 1; i <= 20; i++){
+    console.log(i);
+}
+for(let i = 1; i <= 20; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
 
-// for(let i = 1; i <= 20; i++){
-//     if(i == 10){
-//         break;
-//     }
-//     console.log(i);
-// }
-
-
-// for(let i = 1; i <= 20; i++){
-//     if(i == 5){
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-// let cities = [
-//     "lahore",
-//     "islamabad",
-//     "karachi",
-//     "narowal",
-//     "washingtan"
-// ];
-
-// for(let city of cities){
-//     console.log(city);
-// }
-
-// let car = {
-//     brand: "Toyota",
-//     model: "Corolla",
-//     year: 2025
-// };
-
-// for(let key in car){
-//     console.log(key, car[key]);
-// }
-
-// function multiply(a, b){
-//     return a*b;
-// }
-
-// let result = multiply(2,2);
-// console.log(result)
-
-// function iseven(number){
-//     if(number % 2 == 0){
-//         return console.log(true);
-//     } else {
-//         return console.log(false);
-//     }
-// }
-
-// iseven(3);
-
-// function findlargest(a, b){
-//     if(a > b){
-//         return console.log(a + " is largest");
-//     } else {
-//         return console.log(b + " is largest");
-//     }
-// }
-
-// findlargest(4, 5);
-
-// function calculateArea(heigth, width){
-//     return console.log("Area of the rectangle is " + heigth * width);
-// }
-
-// calculateArea(12, 12);
+for(let i = 1; i <= 20; i++){
+    if(i == 10){
+        break;
+    }
+    console.log(i);
+}
 
 
-// function fullName(firstname, lastname){
-//     return console.log(firstname + " " + lastname);
-// }
+for(let i = 1; i <= 20; i++){
+    if(i == 5){
+        continue;
+    }
+    console.log(i);
+}
 
-// fullName("uzair", "khan");
+let cities = [
+    "lahore",
+    "islamabad",
+    "karachi",
+    "narowal",
+    "washingtan"
+];
 
-// let fruits = [
-//     "Apple",
-//     "Mango",
-//     "Banana",
-//     "Orange"
-// ];
+for(let city of cities){
+    console.log(city);
+}
 
-// let [one, two, three, four] = fruits;
-// console.log(one);
-// console.log(two);
-// console.log(three);
-// console.log(four);
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2025
+};
 
-// let [_one,,_two,_three] = fruits;
-// console.log(_one);
-// console.log(_two);
-// console.log(_three);
+for(let key in car){
+    console.log(key, car[key]);
+}
 
-// let laptop = {
-//     brand: "HP",
-//     model: "EliteBook",
-//     price: 67000
-// };
+function multiply(a, b){
+    return a*b;
+}
 
-// let { brand, model, price } = laptop;
-// console.log(brand);
-// console.log(model);
-// console.log(price);
+let result = multiply(2,2);
+console.log(result)
 
-// let {brand: company } = laptop;
-// console.log(company);
+function iseven(number){
+    if(number % 2 == 0){
+        return console.log(true);
+    } else {
+        return console.log(false);
+    }
+}
 
-// let {
-//     color = "black"
-// } = laptop;
-// console.log(color);
+iseven(3);
 
-// let numbers = [1, 2, 3];
-// let [...others] = numbers;
-// console.log(others);
+function findlargest(a, b){
+    if(a > b){
+        return console.log(a + " is largest");
+    } else {
+        return console.log(b + " is largest");
+    }
+}
 
-// let a = [1, 2];
-// let b = [3, 4];
+findlargest(4, 5);
 
-// let ab = [
-//     ...a,
-//     ...b
-// ];
-// console.log(ab);
+function calculateArea(heigth, width){
+    return console.log("Area of the rectangle is " + heigth * width);
+}
 
-// let person = {
-//     name: "Ali",
-//     age: 22
-// };
+calculateArea(12, 12);
 
-// let {...copy} = person;
-// console.log(copy);
 
-// let updated =  {
-//     ...person,
-//     age: 23
-// };
-// console.log(updated);
+function fullName(firstname, lastname){
+    return console.log(firstname + " " + lastname);
+}
 
-// function multiply(...numbers){
-//     mul = 1;
-//     for(let number of numbers){
-//         mul *= number;
-//     }
-//     return mul;
-// }
+fullName("uzair", "khan");
 
-// console.log(multiply(1,2,3,4,5,6));
+let fruits = [
+    "Apple",
+    "Mango",
+    "Banana",
+    "Orange"
+];
+
+let [one, two, three, four] = fruits;
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+
+let [_one,,_two,_three] = fruits;
+console.log(_one);
+console.log(_two);
+console.log(_three);
+
+let laptop = {
+    brand: "HP",
+    model: "EliteBook",
+    price: 67000
+};
+
+let { brand, model, price } = laptop;
+console.log(brand);
+console.log(model);
+console.log(price);
+
+let {brand: company } = laptop;
+console.log(company);
+
+let {
+    color = "black"
+} = laptop;
+console.log(color);
+
+let numbers = [1, 2, 3];
+let [...others] = numbers;
+console.log(others);
+
+let a = [1, 2];
+let b = [3, 4];
+
+let ab = [
+    ...a,
+    ...b
+];
+console.log(ab);
+
+let person = {
+    name: "Ali",
+    age: 22
+};
+
+let {...copy} = person;
+console.log(copy);
+
+let updated =  {
+    ...person,
+    age: 23
+};
+console.log(updated);
+
+function multiply(...numbers){
+    mul = 1;
+    for(let number of numbers){
+        mul *= number;
+    }
+    return mul;
+}
+
+console.log(multiply(1,2,3,4,5,6));
 
